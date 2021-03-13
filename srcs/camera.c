f411f9b92e232c0f79ec2cb4bd6b28abd9c88d07
yassine taji <yassinoubil3@gmail.com>
2021-03-13 18:06:03 +0100

    save

--- srcs/camera.c
+++ srcs/camera.c
@@ -6,7 +6,7 @@
 /*   By: yataji <yataji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 01:02:43 by yataji            #+#    #+#             */
-/*   Updated: 2021/03/13 16:49:16 by yataji           ###   ########.fr       */
+/*   Updated: 2021/03/13 17:03:42 by yataji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void		initcamera(t_cam *cam)
 		cam->v = (t_vect){0, 0, -1 * dot1};
 	else
 		cam->v = (t_vect){0, 1, 0};
-	cam->fov = 60 * M_PI / 180;
+	cam->fov = cam->fov * M_PI / 180;
 	cam->plnh = tan(cam->fov / 2);
 	cam->plnw = cam->plnh * MAXWIDTH / MAXHEIGHT;
 	cam->u = crosspro(cam->v, cam->w);
--- srcs/parse/camera.c
+++ srcs/parse/camera.c
@@ -6,7 +6,7 @@
 /*   By: yataji <yataji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 16:20:09 by yataji            #+#    #+#             */
-/*   Updated: 2021/03/10 19:25:44 by yataji           ###   ########.fr       */
+/*   Updated: 2021/03/13 17:09:06 by yataji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ int			stockcamera(t_cam *cam, char *str)
 	char	**value;
 
 	value = ft_strsplit(str, ':');
-	if (ft_strlend(value) != 4)
+	if (ft_strlend(value) != 4 && ft_strlend(value) != 2)
 		return (-1);
 	if (ft_strcmp(value[0], "\tlokat") == 0)
 		cam->lokat = stk(value);
 	else if (ft_strcmp(value[0], "\tlokfrm") == 0)
 		cam->lokfrm = stk(value);
+	else if (ft_strlend(value) == 2 && ft_strcmp(value[0], "\tfov") == 0)
+		cam->fov = ft_atoi(value[1]);
 	ft_strdel(value);
 	return (1);
 }
@@ -32,7 +34,7 @@ int			camera(t_cam *cam, char *str, int fd)
 	int		l;
 
 	l = 0;
-	while (l < 2)
+	while (l < 3)
 	{
 		if (get_next_line(fd, &str) < 0)
 			return (-1);
@@ -42,6 +44,9 @@ int			camera(t_cam *cam, char *str, int fd)
 			stockcamera(cam, str);
 		else if (checkval(str, "\tlokfrm: ", 9) > 0 ||
 			checkval(str, "\tlokfrm:\t", 9) > 0)
+			stockcamera(cam, str);
+		else if (checkval(str, "\tfov: ", 6) > 0 ||
+			checkval(str, "\tfov:\t", 6) > 0)
 			stockcamera(cam, str);
 		else
 			return (-1);
