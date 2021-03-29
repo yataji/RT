6739625b3cd49b540280f21917b5284d05c0c8f9
yassine taji <yassinoubil3@gmail.com>
2021-03-29 14:36:30 +0100

    syntax yaml and change code for sytax yaml

--- srcs/parse/obj_plan.c
+++ srcs/parse/obj_plan.c
@@ -29,13 +29,13 @@ int			stockplan(t_obj *obj, char *str)
 	value = ft_strsplit(str, ':');
 	if (ft_strlend(value) != 4)
 		return (-1);
-	else if (ft_strcmp(value[0], "\tcenter") == 0)
+	else if (ft_strcmp(value[0], " center") == 0)
 		obj->center = stk(value);
-	else if (ft_strcmp(value[0], "\tcolor") == 0)
+	else if (ft_strcmp(value[0], " color") == 0)
 		obj->color = stk(value);
-	else if (ft_strcmp(value[0], "\taxis") == 0)
+	else if (ft_strcmp(value[0], " axis") == 0)
 		obj->v = stk(value);
-	else if (ft_strcmp(value[0], "\trot") == 0)
+	else if (ft_strcmp(value[0], " rot") == 0)
 		obj->rot = stk(value);
 	ft_strdel(value);
 	return (1);
@@ -50,13 +50,13 @@ int			plan(t_obj *obj, char *str, int fd)
 	{
 		if (get_next_line(fd, &str) < 0)
 			return (-1);
-		if (ck(str, "\tcenter: ", 9) > 0 || ck(str, "\tcenter:\t", 9) > 0)
+		if (ck(str, " center: ", 9) > 0 || ck(str, " center: ", 9) > 0)
 			stockplan(obj, str);
-		else if (ck(str, "\tcolor: ", 8) > 0 || ck(str, "\tcolor:\t", 8) > 0)
+		else if (ck(str, " color: ", 8) > 0 || ck(str, " color: ", 8) > 0)
 			stockplan(obj, str);
-		else if (ck(str, "\taxis: ", 7) > 0 || ck(str, "\taxis:\t", 7) > 0)
+		else if (ck(str, " axis: ", 7) > 0 || ck(str, " axis: ", 7) > 0)
 			stockplan(obj, str);
-		else if (ck(str, "\trot: ", 6) > 0 || ck(str, "\trot:\t", 6) > 0)
+		else if (ck(str, " rot: ", 6) > 0 || ck(str, " rot: ", 6) > 0)
 			stockplan(obj, str);
 		else
 			return (-1);
